c86b260794ca714fff3690dd4615a23c9a1490d7
juanvillago <juanvillarroelcontacto@gmail.com>
2024-09-19 15:22:40 -0400

    primer commit

---
+++ home.php
@@ -0,0 +1,18 @@
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+<h1>BIENVENIDOS</h1>
+    
+click aqui para <a href="logout.php">Cerrar Sesion</a>
+
+
+</body>
+</html>
